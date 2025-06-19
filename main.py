8b406e653a44594735ef3f90bce4476cc5b5e71c
AliuQudus <qudusaliu98@gmail.com>
2025-06-19 21:59:27 +0100

    add Body import to handle request body in createPost endpoint

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Body
 
 app = FastAPI()
 
@@ -13,3 +13,16 @@ def getPost():
     return {"data":
             {"Name": "Aliu Qudus", "Age": 27, "Gender": "Male", "Country": "Nigeria"}
             }
+
+
+@app.post("/create")
+def createPost(post: dict = Body(...)):
+    print(post)
+    return {
+        # "content": {
+        #     "title": "Introduction",
+        #     "content": "Hello, world! This is my first social media post. I hope you will follow me for more of my contents.",
+        #     "user_id": "qudusaliu@gmail.com"
+        # },
+        "Message": "Post Successfully Created"
+    }
