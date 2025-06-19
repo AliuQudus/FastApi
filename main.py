320d6cc685bb42b5105012418b506de64c4f915c
AliuQudus <qudusaliu98@gmail.com>
2025-06-19 20:56:13 +0100

    update root endpoint message and add new post endpoint

--- main.py
+++ main.py
@@ -5,4 +5,11 @@ app = FastAPI()
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return {"message": "Welcome to my World!!!"}
+
+
+@app.get("/post")
+def getPost():
+    return {"data":
+            {"Name": "Aliu Qudus", "Age": 27, "Gender": "Male", "Country": "Nigeria"}
+            }
