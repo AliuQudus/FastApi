ae3682128e3c9b0890059acbae0cf638b9466b8e
AliuQudus <qudusaliu98@gmail.com>
2025-08-15 15:21:39 +0100

    feat: initialize database connection setup with SQLAlchemy

---
+++ app/database.py
@@ -0,0 +1,13 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+# Sql_database = 'postgresql://<username>:<password>@<hostname>/<database_name>'
+
+Sql_database = "postgresql://postfres:quhduzski@localhost/Fastapi"
+
+engine = create_engine(Sql_database)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
