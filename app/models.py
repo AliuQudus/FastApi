b32addadd8c02ac29fc8eb62ade23fdcc5b243bc
AliuQudus <qudusaliu98@gmail.com>
2025-08-16 06:43:09 +0100

    refactor: update Post model with improved column definitions and default values

--- app/models.py
+++ app/models.py
@@ -1,13 +1,18 @@
-from sqlalchemy import Column, Integer, String, Boolean
+from pydoc import text
+from tkinter import Text
+from sqlalchemy import TIMESTAMP, Column, Integer, text, String, Boolean
 from .database import Base
 
 
 class Post(Base):
     __tablename__ = "post"
 
-    id = Column(Integer, nullable=False)
-    username = Column(String, nullable=False, primary_key=True)
+    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
+    username = Column(String, nullable=False, unique=True)
     title = Column(String, nullable=False)
     content = Column(String, nullable=False)
     rating = Column(String, nullable=True)
-    published = Column(Boolean, default=True)
+    published = Column(Boolean, server_default="True", nullable=False)
+    created_at = Column(
+        TIMESTAMP(timezone=True), nullable=False, server_default=text("now()")
+    )
