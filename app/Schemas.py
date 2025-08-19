55cec31afa04a3a36a542a78212d9a23c022bf0b
AliuQudus <qudusaliu98@gmail.com>
2025-08-19 05:22:25 +0100

    refactor: add Schemas.py for post creation and update models

---
+++ app/Schemas.py
@@ -0,0 +1,16 @@
+from pydantic import BaseModel
+from typing import Optional
+
+
+class createPost(BaseModel):
+    username: str
+    title: str
+    content: str
+    rating: Optional[float] = None
+    published: Optional[bool] = None
+
+
+class UpdatePost(BaseModel):
+    title: Optional[str] = None
+    content: Optional[str] = None
+    rating: Optional[float] = None
