e676c3464eaf0b83a3fb66d98e05fbc1b492f566
AliuQudus <qudusaliu98@gmail.com>
2025-08-22 08:16:41 +0100

    Task: Created a new table in SQL for users

--- app/models.py
+++ app/models.py
@@ -16,3 +16,15 @@ class Post(Base):
     created_at = Column(
         TIMESTAMP(timezone=True), nullable=False, server_default=text("now()")
     )
+
+
+class Login(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
+    username = Column(String, nullable=False, unique=True)
+    email = Column(String, nullable=False, unique=True)
+    password = Column(String, nullable=False)
+    created_at = Column(
+        TIMESTAMP(timezone=True), nullable=False, server_default=text("now()")
+    )
