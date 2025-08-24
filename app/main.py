9d9f0636122f29f1e3558ce416bf607160d08a51
AliuQudus <qudusaliu98@gmail.com>
2025-08-24 21:30:47 +0100

    Task: Section each endpoint based on their usage and added a router part to access each APIs

--- app/main.py
+++ app/main.py
@@ -1,15 +1,13 @@
-from ast import And, Or
 from email.policy import HTTP
-from fastapi import Depends, FastAPI, Body, HTTPException, status, Path
-from fastapi.responses import JSONResponse
-from fastapi.encoders import jsonable_encoder
+from fastapi import Depends, FastAPI
 import psycopg2
 from psycopg2.extras import RealDictCursor
 from sqlalchemy.orm import Session
 from passlib.context import CryptContext
 import time
-from . import Schemas, models
+from . import models
 from .database import engine, get_db
+from .routers import post, user, auth
 
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
@@ -39,166 +37,12 @@ while True:  # This is to keep the code running until it connects
         time.sleep(5)  # The time it will wait before trying to reconnect
 
 
+app.include_router(post.router)
+app.include_router(user.router)
+app.include_router(auth.router)
+
+
 @app.get("/sqlachemy")
 def sql_alchemy(db: Session = Depends(get_db)):
 
     return {"Status": "Successful"}
-
-
-@app.get(
-    "/posts",
-    response_model=list[Schemas.Response],
-    response_model_exclude_none=True,  # This excludes any null value in the response
-)
-def getPost(db: Session = Depends(get_db)):
-
-    post = db.query(models.Post).all()
-    return post
-
-
-@app.post(
-    "/posts", response_model=Schemas.Response, status_code=status.HTTP_201_CREATED
-)
-def createPost(post: Schemas.createPost, db: Session = Depends(get_db)):
-
-    existing_user = (
-        db.query(models.Post).filter(models.Post.username == post.username).first()
-    )
-    if existing_user:
-        return JSONResponse(
-            status_code=status.HTTP_200_OK,
-            content=jsonable_encoder(
-                {"message": "User already exists", "detail": post.username}
-            ),
-        )
-
-    # new_post = models.Post(
-    #     username=post.username,
-    #     title=post.title,
-    #     content=post.content,
-    #     rating=post.rating,
-    #     published=post.published,
-    # )
-
-    new_post = models.Post(
-        **post.model_dump()
-    )  # This is same as the commented code above, it only looks cleaner than the above.
-
-    db.add(new_post)
-    db.commit()
-    db.refresh(new_post)
-
-    return new_post
-
-
-@app.get("/posts/{username}", response_model=Schemas.Response)
-def getPost(
-    username: str = Path(..., pattern="^[A-Za-z_ ]+$"), db: Session = Depends(get_db)
-):  # The path restrict the input to str
-    getpost = db.query(models.Post).filter(models.Post.username == username).first()
-
-    if not getpost:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND,
-            detail=f"No posts found for username '{username}'",
-        )
-    return getpost
-
-
-@app.delete("/posts/{username}", status_code=status.HTTP_200_OK)
-# The path restrict the input to str
-def deletePost(
-    username: str = Path(..., pattern="^[A-Za-z_ ]+$"), db: Session = Depends(get_db)
-):
-
-    post = db.query(models.Post).filter(models.Post.username == username)
-
-    if post.first() == None:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND,
-            detail=f"No post found for username '{username}'",
-        )
-
-    post.delete(synchronize_session=False)
-
-    db.commit()
-
-    return {
-        "message": f"Post by '{username}' has been successfully deleted",
-        **jsonable_encoder(post),
-    }
-
-
-@app.put("/posts/{username}", response_model=Schemas.Response)
-def updatePost(
-    username: str = Path(..., regex="^[A-Za-z_ ]+$"),
-    post: Schemas.UpdatePost = Body(...),
-    db: Session = Depends(get_db),
-):
-    post_query = db.query(models.Post).filter(models.Post.username == username)
-    db_post = post_query.first()
-
-    if db_post is None:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND,
-            detail=f"No post found for username '{username}'",
-        )
-
-    post_query.update(post.model_dump(), synchronize_session=False)
-
-    db.commit()
-    db.refresh(db_post)
-
-    return {
-        "message": f"Post by '{username}' was successfully updated.",
-        **db_post.__dict__,
-    }
-
-
-@app.get(
-    "/users",
-    status_code=status.HTTP_201_CREATED,
-    response_model=list[Schemas.UserResponse],
-)
-def getPost(db: Session = Depends(get_db)):
-
-    post = db.query(models.Login).all()
-    return post
-
-
-@app.post(
-    "/users", status_code=status.HTTP_201_CREATED, response_model=Schemas.UserResponse
-)
-def createAccount(post: Schemas.Login, db: Session = Depends(get_db)):
-
-    existing_user = (
-        db.query(models.Login).filter(models.Login.email == post.email).first()
-    )
-    if existing_user:
-        return JSONResponse(
-            status_code=status.HTTP_200_OK,
-            content=jsonable_encoder(
-                {"message": "Account already exists", "detail": post.email}
-            ),
-        )
-
-    Or
-
-    existing_user = (
-        db.query(models.Login).filter(models.Login.username == post.username).first()
-    )
-    if existing_user:
-        return JSONResponse(
-            status_code=status.HTTP_200_OK,
-            content=jsonable_encoder(
-                {"message": "User already exists", "detail": post.username}
-            ),
-        )
-
-    new_user = models.Login(**post.model_dump())
-
-    db.add(new_user)
-    db.commit()
-    db.refresh(new_user)
-
-    return new_user
