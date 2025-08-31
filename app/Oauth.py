98f3678d4b8d80f728745a02adb1963043819fab
AliuQudus <qudusaliu98@gmail.com>
2025-08-31 15:00:46 +0100

    Task: Implemant a JWT and verification functions

--- app/Oauth.py
+++ app/Oauth.py
@@ -1,5 +1,10 @@
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, timezone
+from fastapi import Depends, HTTPException, status
 from jose import JWTError, jwt
+from app import Schemas
+from fastapi.security import OAuth2PasswordBearer
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/login")
 
 Secret_key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c"
 
@@ -11,9 +16,36 @@ Token_Expiration = 60
 def AccessToken(data: dict):
     encode = data.copy()
 
-    expiration = datetime.now() + timedelta(minutes=Token_Expiration)
-    encode.update({"exp": expiration})
+    expiration = datetime.now(timezone.utc) + timedelta(minutes=Token_Expiration)
+    # encode.update({"exp": expiration})
+    encode.update({"exp": int(expiration.timestamp())})
 
     encoded_jwt = jwt.encode(encode, Secret_key, algorithm=Algorithm)
 
     return encoded_jwt
+
+
+def VerifyToken(token: str, credentials_exception):
+
+    try:
+        payload = jwt.decode(token, Secret_key, algorithms=Algorithm)
+
+        username: str = payload.get("username")
+
+        if username == None:
+            raise credentials_exception
+        token_data = Schemas.TokenData(username=username)
+    except JWTError:
+        raise credentials_exception
+
+    return token_data
+
+
+def getCurrentUser(token: str = Depends(oauth2_scheme)):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail=f"Could not validate credentials",
+        headers={"WWW.Authenticate": "Bearer"},
+    )
+
+    return VerifyToken(token, credentials_exception)
