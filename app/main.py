5ba5b624741fe96d560531f2f7851811bc8fd3e3
AliuQudus <qudusaliu98@gmail.com>
2025-09-10 15:55:44 +0100

    Task: Added a new router for like

--- app/main.py
+++ app/main.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import Session
 from passlib.context import CryptContext
 from . import models
 from .database import engine, get_db
-from .routers import post, user, auth
+from .routers import post, user, auth, like
 
 
 models.Base.metadata.create_all(bind=engine)
@@ -14,6 +14,7 @@ app = FastAPI()
 app.include_router(post.router)
 app.include_router(user.router)
 app.include_router(auth.router)
+app.include_router(like.router)
 
 
 @app.get("/sqlachemy")
