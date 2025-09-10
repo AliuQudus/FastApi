e0c77fb4c885dd6786d671ea22de1b705318954b
AliuQudus <qudusaliu98@gmail.com>
2025-09-10 15:58:18 +0100

    Task: Created a new  logic for liking a post

---
+++ app/routers/like.py
@@ -0,0 +1,55 @@
+from fastapi import APIRouter, Depends, FastAPI, Body, HTTPException, status, Path
+from .. import Schemas, database, models, Oauth
+from sqlalchemy.orm import Session
+
+
+router = APIRouter(prefix="/like", tags=["Likes"])
+
+
+@router.post("", status_code=status.HTTP_200_OK)
+def likes(
+    vote: Schemas.Likes,
+    db: Session = Depends(database.get_db),
+    current_user: Schemas.TokenData = Depends(Oauth.getCurrentUser),
+):
+
+    query = db.query(models.Like).filter(
+        models.Like.post_id == vote.post_id,
+        models.Like.user_username == current_user.username,
+    )
+    liked = query.first()
+
+    post = db.query(models.Post).filter(models.Post.id == vote.post_id).first()
+
+    if not post:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"Post with id '{vote.post_id}' does not exist",
+        )
+
+    if vote.dir == 1:
+        if liked:
+            raise HTTPException(
+                status_code=status.HTTP_409_CONFLICT,
+                detail=f"User '{current_user.username} already like this post '{vote.post_id}'",
+            )
+
+        new_vote = models.Like(
+            post_id=vote.post_id, user_username=current_user.username
+        )
+        db.add(new_vote)
+        db.commit()
+
+        return {"message": "You liked a post"}
+
+    else:
+        if not liked:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail=f"You haven't liked this post yet",
+            )
+
+        query.delete(synchronize_session=False)
+        db.commit()
+
+        return {"message": "You successfully unliked this post"}
